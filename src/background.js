5c4c1d0a9ad023fa6dd354c9e2b398877e98f807
qiancheng.zhao <qiancheng.zhao@alo7.com>
2024-04-19 10:25:14 +0800

    feat: update

--- src/background.js
+++ src/background.js
@@ -1,16 +1,40 @@
 import { Action } from "./common";
 import { AI_PROMPT_SEARCH, AI_SEARCH, STOP, ZERO_LOG } from "./common/constant";
+/**
+ * @description 插件标签打开侧边栏
+ */
 chrome.sidePanel
   .setPanelBehavior({ openPanelOnActionClick: true })
   .catch((error) => console.error(error));
-chrome.runtime.onInstalled.addListener(function () {
+
+/**
+ * @description 监听快捷键
+ */
+chrome.commands.onCommand.addListener((command) => {
+  switch (command) {
+    case "open_side_panel":
+      chrome.windows.getCurrent((currentWindow) => {
+        chrome.sidePanel.open({ windowId: currentWindow.id });
+      });
+      break;
+  }
+});
+
+/**
+ * @description 创建右键菜单项
+ */
+chrome.runtime.onInstalled.addListener(function (details) {
   // 创建一级菜单项
   chrome.contextMenus.create({
     id: AI_SEARCH,
     title: "AI搜索",
-    contexts: ["all"],
+    contexts: ["selection"],
   });
 });
+
+/**
+ * @description 右键菜单项点击事件 触发AI搜索
+ */
 chrome.contextMenus.onClicked.addListener(async function (info, tab) {
   if (info.menuItemId === AI_SEARCH) {
     await chrome.sidePanel.open({ windowId: tab.windowId });
@@ -18,12 +42,20 @@ chrome.contextMenus.onClicked.addListener(async function (info, tab) {
   }
 });
 
+/**
+ * @description 监听日志输出
+ */
 chrome.runtime.onMessage.addListener(function (action) {
   switch (action.type) {
     case ZERO_LOG:
       console.log(action.type, action.payload);
   }
 });
+
+/**
+ * @description 监听Prompt AI搜索
+ 
+ */
 chrome.runtime.onMessage.addListener(function (action) {
   switch (action.type) {
     case AI_PROMPT_SEARCH:
@@ -35,14 +67,16 @@ chrome.runtime.onMessage.addListener(function (action) {
       });
   }
 });
+
+/**
+ * @description 监听停止AI搜索
+ 
+ */
 chrome.runtime.onMessage.addListener(function (action) {
   switch (action.type) {
     case STOP:
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(
-          tabs[0].id,
-          new Action(STOP, action.payload)
-        );
+        chrome.tabs.sendMessage(tabs[0].id, new Action(STOP, action.payload));
       });
   }
 });
